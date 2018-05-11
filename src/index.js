fed49b346ba3219c622929cccbe0a5c5b84814e7
Lus <dals@gft.com>
2018-05-11 12:01:28 +0200

    seperate state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
-import {createStore} from 'redux'
+
 import { Provider } from 'react-redux';
-import reducer from './reducer';
 
-const store = createStore(reducer);
-
-//'store.dispatch({type: "INCREMENT"});
-// store.dispatch({type: "DECREMENT"});
+import {store} from './store';
 
 const App = () => (
   <Provider store={store}>
